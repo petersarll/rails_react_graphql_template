0f3dc391eb272c28bfd39cb551501b23770bdd68
Peter Sarll <petersarll@gmail.com>
2020-02-29 23:27:39 -0800

    changed exercise_name to exerciseName

--- rails_react_graphql/db/seeds.rb
+++ rails_react_graphql/db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Workout.create(
-  exercise_name: "Barbell Squat",
+  exerciseName: "Barbell Squat",
   sets: "6",
   reps: "10",
   rest: "1 minute"
 )
 
 Workout.create(
-  exercise_name: "Dumbell Thruster",
+  exerciseName: "Dumbell Thruster",
   sets: "3",
   reps: "15",
   rest: "30 seconds"
