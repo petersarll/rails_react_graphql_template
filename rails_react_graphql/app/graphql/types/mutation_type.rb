499c12f0512dd36409e02f2b72991c29f55e734f
Peter Sarll <petersarll@gmail.com>
2020-03-04 15:22:58 -0800

    adding the destroy element in graphQL

--- rails_react_graphql/app/graphql/types/mutation_type.rb
+++ rails_react_graphql/app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :destroy_exercise, mutation: Mutations::DestroyExercise
     field :create_exercise, mutation: Mutations::CreateExercise
   end
 end
