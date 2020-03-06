bd836a6bb33de81fead5fbfd388f0138c94f0bcf
Peter Sarll <petersarll@gmail.com>
2020-03-05 19:51:29 -0800

    adding better stylization and added svg file to app.js

--- rails_react_graphql/app/javascript/src/App.js
+++ rails_react_graphql/app/javascript/src/App.js
@@ -3,6 +3,7 @@ import { ApolloProvider } from 'react-apollo'
 import { client } from './apollo'
 import WorkoutComponent from './Components/WorkoutComponent'
 import WorkoutList from './Components/WorkoutList'
+import CoolLogo from './mockup.svg'
 
 import './stylesheets/App.css'
 
@@ -15,10 +16,11 @@ class App extends Component {
             <div className="mp-2" id="ht"></div>
             <div className="mp-3"></div>
             <div className="mp-4" id="ht"></div>
-            {/* <header className="mp-h1">ReTrain</header> */}
-            {/* <WorkoutComponent />
-                    <WorkoutList /> */}
           </header>
+          <div className="mp-h1">Welcome</div>
+          <img className="laptop" src={CoolLogo} />
+          {/* <WorkoutComponent />
+          <WorkoutList /> */}
         </div>
       </ApolloProvider>
     )
