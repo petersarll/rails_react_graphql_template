2b8775c4f4e4c1bf9ef625db04fd9e86744bf0c4
Peter Sarll <petersarll@gmail.com>
2020-03-11 18:28:02 -0700

    creating filter method to search through exercises

--- rails_react_graphql/app/javascript/src/App.js
+++ rails_react_graphql/app/javascript/src/App.js
@@ -3,6 +3,7 @@ import { ApolloProvider } from 'react-apollo'
 import { client } from './apollo'
 import WorkoutComponent from './Components/WorkoutComponent'
 import WorkoutList from './Components/WorkoutList'
+import List from './Components/List'
 import { HashLink as Link } from 'react-router-hash-link'
 import { BrowserRouter as Router } from 'react-router-dom'
 
@@ -13,13 +14,63 @@ import './stylesheets/App.css'
 import 'animate.css/animate.min.css'
 import ScrollAnimation from 'react-animate-on-scroll'
 
+import { jobs } from './exerciseLibrary.js'
+
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      list: jobs
+    }
+    // this.addItem = this.addItem.bind(this)
+    // this.removeItem = this.removeItem.bind(this)
   }
 
+  // addItem(e) {
+  //   // Prevent button click from submitting form
+  //   e.preventDefault()
+
+  //   // Create variables for our list, the item to add, and our form
+  //   let list = Sample
+  //   const newItem = document.getElementById('addInput')
+  //   const form = document.getElementById('addItemForm')
+
+  //   // If our input has a value
+  //   if (newItem.value != '') {
+  //     // Add the new item to the end of our list array
+  //     list.push(newItem.value)
+  //     // Then we use that to set the state for list
+  //     this.setState({
+  //       list: list
+  //     })
+  //     // Finally, we need to reset the form
+  //     newItem.classList.remove('is-danger')
+  //     form.reset()
+  //   } else {
+  //     // If the input doesn't have a value, make the border red since it's required
+  //     newItem.classList.add('is-danger')
+  //   }
+  // }
+
+  // removeItem(item) {
+  //   // Put our list into an array
+  //   const list = this.state.list.slice()
+  //   // Check to see if item passed in matches item in array
+  //   list.some((el, i) => {
+  //     if (el === item) {
+  //       // If item matches, remove it from array
+  //       list.splice(i, 1)
+  //       return true
+  //     }
+  //   })
+  //   // Set state to list
+  //   this.setState({
+  //     list: list
+  //   })
+  // }
+
   render() {
+    console.log(this.state.list)
     return (
       <ApolloProvider client={client}>
         <Router>
@@ -66,6 +117,28 @@ class App extends Component {
           <WorkoutComponent />
           <WorkoutList />
           <img className="ipad" src={Ipad} />
+          {/* adding exercise */}
+          <div className="content">
+            <div className="container">
+              <section className="section">
+                <List items={this.state.list} delete={this.removeItem} />
+              </section>
+              <hr />
+              <section className="section">
+                <form className="form" id="addItemForm">
+                  {/* <input
+                    type="text"
+                    className="input"
+                    id="addInput"
+                    placeholder="Something that needs to be done..."
+                  />
+                  <button className="button is-info" onClick={this.addItem}>
+                    Add Item
+                  </button> */}
+                </form>
+              </section>
+            </div>
+          </div>
           <div className="contact-div" id="contactnow"></div>
         </Router>
       </ApolloProvider>
